a62b483039ff4ed8366fc3acfbf6a2d788f382cc
wings409 <57186193+wings409@users.noreply.github.com>
2020-07-07 16:10:57 +0800

    Update 60_a.py

--- sys/ntp/60_a.py
+++ sys/ntp/60_a.py
@@ -1,1 +1,1 @@
-[{"endpoint": "host01", "tags": "", "timestamp": 1431349763, "metric": "sys.ntp.offset", "value": 0.73699999999999999, "counterType": "GAUGE", "step": 600}]
+[{"endpoint": "host01", "tags": "", "timestamp": 1431349763, "metric": "sys.ntp.offset", "value": 0.73699999999999999, "counterType": "GAUGE", "step": 60}]
